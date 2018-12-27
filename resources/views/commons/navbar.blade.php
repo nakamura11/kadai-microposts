f95919bd661854bca2ad28181c49cb2d700a60c3
nononono <meisou0308@yahoo.co.jp>
2018-12-27 12:59:41 +0000

    top page

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,17 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expend-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Microposts</a>
+        
+        <button type="botton" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-time"><a href="#" class="nav-link">Signup</a></li>
+                <li class="nav-time"><a href="#" class="nav-link">Login</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
